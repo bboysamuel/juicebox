65fb3b76573dcbed45fabc78d445645c9d351404
samuel mckaig <bboysamuel@hotmail.com>
2020-09-27 23:39:14 -0700

    added some heroku stuff

--- db/index.js
+++ db/index.js
@@ -1,4 +1,8 @@
-const { Client } = require('pg'); // imports the pg module
+// const { Client } = require('pg'); // imports the pg module
+
+const client = new Client(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev'
+);
 
 require('dotenv').config();
 
